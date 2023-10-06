d599d753c783457918924b478b72a7a3eb79f2e3
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2023-10-06 11:49:43 -0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Context provider
 import { AppProvider } from "./context/AppProvider";
 
-const App: React.FC = () => {
+// Pages
+import { HomePage } from "./pages/home.page";
+import { ProfilePage } from "./pages/profile.page";
+import { NotFoundPage } from "./pages/404.page";
+
+export const App: React.FC = () => {
   return (
     <AppProvider>
-      <h1>Motorcycle blog</h1>
+      <BrowserRouter>
+        <h1>Motorcycle blog</h1>
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/:username" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </AppProvider>
   );
 };
-
-export default App;
